43996f205dc9dee476a445936da00146f359703e
meValverde <90765194+meValverde@users.noreply.github.com>
2021-11-03 14:58:18 +0100

    10

--- api/BerryRecog.cs
+++ api/BerryRecog.cs
@@ -28,12 +28,14 @@ namespace BerryApp
             string url = "https://berryprediction-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/1cd03d8e-e79a-4e17-ba18-a6d672ccd759/classify/iterations/berryR/url";      
 
             string someUrl="https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg";
+            
+            HttpResponseMessage response;
             byte[] byteData = webClient.DownloadData(someUrl);
 
             using (var content = new ByteArrayContent(byteData))
             {
                 content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                var response = await client.PostAsync(url, content);
+                response = await client.PostAsync(url, content);
                 
             }
 

a5005485ee538c03a0384b02a8f8a3fd6378713f
meValverde <90765194+meValverde@users.noreply.github.com>
2021-11-04 21:01:53 +0100

    jk

--- api/BerryRecog.cs
+++ api/BerryRecog.cs
@@ -25,24 +25,26 @@ namespace BerryApp
             var queryString = HttpUtility.ParseQueryString(string.Empty);
 
             client.DefaultRequestHeaders.Add("Prediction-Key", "c577dc58f5374413a3fea829c4938399");
-            string someUrl='{"Url":"https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg"}';
+            string someUrl="https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg";
 
-            queryString["application"] = "{string}";
-            string uri = "https://berryprediction-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/1cd03d8e-e79a-4e17-ba18-a6d672ccd759/classify/iterations/berryR/url?"+ queryString;   
+            queryString["application"] = someUrl;
+            string uri = "https://berryprediction-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/1cd03d8e-e79a-4e17-ba18-a6d672ccd759/classify/iterations/berryR/url"+ queryString;   
 
-           
-            
             HttpResponseMessage response;
-            byte[] byteData = Encoding.UTF8.GetBytes(someUrl);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            response = await client.PostAsync(uri, someUrl);
+            
+            
+           // byte[] byteData = Encoding.UTF8.GetBytes(someUrl);
 
-            using (var content = new ByteArrayContent(byteData))
-            {
-                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                response = await client.PostAsync(uri, content);
+           // using (var content = new ByteArrayContent(byteData))
+           // {
+            //    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+              //  response = await client.PostAsync(uri, content);
                 
-            }
+           // }
 
-            return new OkObjectResult(response);
+            
         }
             
               

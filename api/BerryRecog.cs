245b3c61a36a8e4e9abc59fa29cacc27227d820d
meValverde <90765194+meValverde@users.noreply.github.com>
2021-11-04 21:11:54 +0100

    e

--- api/BerryRecog.cs
+++ api/BerryRecog.cs
@@ -18,8 +18,7 @@ namespace BerryApp
     {
         [FunctionName("BerryRecog")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
         {
             var client = new HttpClient();
             var queryString = HttpUtility.ParseQueryString(string.Empty);

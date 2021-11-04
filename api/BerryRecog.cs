d9e46914ff20f20f67c21e0ac02ce4894134d9ac
meValverde <90765194+meValverde@users.noreply.github.com>
2021-11-04 21:42:01 +0100

    url

--- api/BerryRecog.cs
+++ api/BerryRecog.cs
@@ -24,7 +24,7 @@ namespace BerryApp
             var queryString = HttpUtility.ParseQueryString(string.Empty);
 
             client.DefaultRequestHeaders.Add("Prediction-Key", "c577dc58f5374413a3fea829c4938399");
-            string someUrl="https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg";
+            //string someUrl="https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg";
 
            // queryString["application"] = someUrl;
             string uri = "https://berryprediction-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/1cd03d8e-e79a-4e17-ba18-a6d672ccd759/classify/iterations/berryR/url"; 
@@ -33,7 +33,7 @@ namespace BerryApp
     
             
             
-            byte[] byteData = Encoding.UTF8.GetBytes(someUrl);
+            byte[] byteData = Encoding.UTF8.GetBytes("{ \"url\": \"https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg" }");
 
             using (var content = new ByteArrayContent(byteData))
             {

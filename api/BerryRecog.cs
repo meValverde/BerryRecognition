13a35a804406f560dec89f93d4a83009ae9089fd
meValverde <90765194+meValverde@users.noreply.github.com>
2021-11-07 14:25:30 +0100

    body3

--- api/BerryRecog.cs
+++ api/BerryRecog.cs
@@ -31,10 +31,10 @@ namespace BerryApp
                 req.Body.CopyTo(mem);
                 body = reader.ReadToEnd();
                 mem.Seek(0, SeekOrigin.Begin);
-                body = reader.ReadToEnd();
+                body = reader.ReadAsStringAsync();
             }
             
-            Console.Write(reqbody);
+            Console.Write(body);
 
            //client.DefaultRequestHeaders.Add("Prediction-Key", "c577dc58f5374413a3fea829c4938399");
             //string someUrl="https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg";
@@ -51,7 +51,7 @@ namespace BerryApp
     
             //string imageUrlex = "https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg";
             string responseBody;
-            byte[] byteData = Encoding.UTF8.GetBytes("{\""+reqbodyy+"\" }");
+            byte[] byteData = Encoding.UTF8.GetBytes("{\""+body+"\" }");
 
             using (var content = new ByteArrayContent(byteData))
             {

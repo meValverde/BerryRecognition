914f76993a2fc1ad22f21cd3d5dabd61fddcfa01
meValverde <90765194+meValverde@users.noreply.github.com>
2021-11-08 22:04:00 +0100

    key

--- api/BerryRecog.cs
+++ api/BerryRecog.cs
@@ -32,7 +32,7 @@ namespace BerryApp
 
             
 
-           //client.DefaultRequestHeaders.Add("Prediction-Key", "c577dc58f5374413a3fea829c4938399");
+           client.DefaultRequestHeaders.Add("Prediction-Key", "c577dc58f5374413a3fea829c4938399");
             //string someUrl="https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg";
 
            // queryString["application"] = someUrl;

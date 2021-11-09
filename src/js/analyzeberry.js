333604b5fce1a06abeb12262b2d8f58db924f10d
meValverde <90765194+meValverde@users.noreply.github.com>
2021-11-09 12:30:25 +0100

    body

--- src/js/analyzeberry.js
+++ src/js/analyzeberry.js
@@ -11,9 +11,9 @@ function analyzeImage() {
         return;
     }
 
-   //const jsonBodyItem = {
-       // imageUrl: imageUrl
-  // };
+    const jsonBodyItem = {
+       imageUrl: imageUrl
+    };
 
     fetch(uri,
         {
@@ -22,7 +22,7 @@ function analyzeImage() {
               //  'Content-Type': 'application/json',
               //  'Prediction-Key':'c577dc58f5374413a3fea829c4938399'
              // },
-            body:'https://www.jespersplanteskole.dk/media/catalog/product/cache/1/image/450x450/9df78eab33525d08d6e5fb8d27136e95/s/y/symphoricarpos_doorenbosii_white_hedge_79_95_13.jpg'
+            body:JSON.stringify(jsonBodyItem)
             
         })
         
